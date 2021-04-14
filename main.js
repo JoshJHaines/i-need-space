0dbbe4a5b7270e4fb51aa0cc5ab9945cdd49d35f
JoshuaJHaines <joshjhaines3@gmail.com>
2021-04-14 14:36:09 -0400

    tried to start work but can't get momentum yet

---
+++ main.js
@@ -0,0 +1,7 @@
+/********
+ * APIS *
+ ********/
+// Will need to adjust so {search_text} is replaced with address from input
+const geoDude = "https://api.mapbox.com/geocoding/v5/mapbox.places/{search_text}.json"
+
+const address = 'input address from page'
